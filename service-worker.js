10fe3218d967636c7fafcdb67cd287f6265c69ff
saiyavong mitthasone <saiyavongmitthasone@gmail.com>
2019-04-11 10:06:01 +0700

    Site updated: 2019-04-11 10:06:01

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/index.html","2a2e12c8edad17de62354ea4531ac82c"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
